f89f211230f48ea256042f49d50bd9d1591c55ab
vdiefn <ddhkxxhxx@hotmail.com>
2022-11-11 18:33:54 +0800

    feat: project init

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const app = express()
-const db = mongoose.connection
+const port = 3000
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.get('/', (req, res) => {
   res.send('Express is operated')
 })
 
-db.on('error', () => {
-  console.log('mongodb error!')
-
-  db.once('open', ()=> {
-    console.log('mongodb connected!')
-  })
-})
-
-app.listen( 3000, () => {
-  console.log('Server is running on port 3000.')
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}.`)
 })
