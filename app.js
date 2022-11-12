da0ef4e16f46d4a794c985be26cb6426591d62db
vdiefn <ddhkxxhxx@hotmail.com>
2022-11-12 09:43:38 +0800

    feat: add todo seeder

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose') // 載入 mongoose
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
 const port = 3000
 
 // 取得資料庫連線狀態
