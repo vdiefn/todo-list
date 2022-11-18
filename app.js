54e0c283e7382b1eb2794efd31bade00c968c8f8
vdiefn <ddhkxxhxx@hotmail.com>
2022-11-18 17:59:58 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
+
+// const mongoose = require('mongoose') // 載入 mongoose
+//一定要這行↓
+// require('dotenv').config({ path: '.env' })
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+
 const exphbs = require("express-handlebars")
 const Todo = require('./models/todo')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const routes = require('./routes')
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+require('./config/mongoose')
 
 const app = express()
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
 
 const port = 3000
 
 // 取得資料庫連線狀態
-const db = mongoose.connection
+// const db = mongoose.connection
 // 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+// db.on('error', () => {
+//   console.log('mongodb error!')
+// })
 // 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+// db.once('open', () => {
+//   console.log('mongodb connected.')
+// })
+
 app.use(methodOverride('_method'))
 
 app.engine("hbs", exphbs({ defaultLayout: 'main', extname: ".hbs" }))
