f316fcb9671823e08806e14d2110c995cf934807
vdiefn <ddhkxxhxx@hotmail.com>
2023-01-03 17:53:59 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-
+//註冊
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+  .then( user => {
+    if (user) {
+      console.log('User already exit.')
+      res.render('register', { name, email, password, confirmPassword})
+    } else {
+     return  User.create({ name, email, password}) 
+      .then( () => {
+        res.redirect('/')
+      })
+      .catch( error => {
+        console.log(error)
+      })
+    }
+  })
 })
 
 router.get('/register', (req, res) => {
